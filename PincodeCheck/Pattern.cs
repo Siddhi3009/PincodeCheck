4b36a4ded2d851a71e146fca972aa54a38929b7e
Administrator <siddhiseth3009@gmail.com>
2020-10-06 21:43:06 +0530

    [Siddhi]Refactor Regex edited to allow space in between pincode

--- PincodeCheck/Pattern.cs
+++ PincodeCheck/Pattern.cs
@@ -7,7 +7,7 @@ namespace PincodeCheck
 {
     class Pattern
     {
-        public static string REGEX_PINCODE = "^[1-9][0-9]{5}$";//400088
+        public static string REGEX_PINCODE = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";//400088
         public bool validatePincode(string pincode)
         {
             return Regex.IsMatch(pincode, REGEX_PINCODE);
