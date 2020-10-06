0ac8d5c3abea060eb3bf93e6e0a38588c6b20ad1
Administrator <siddhiseth3009@gmail.com>
2020-10-06 20:00:49 +0530

    [Siddhi]Add PincodeCheck program added

---
+++ PincodeCheck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PincodeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Pincode Check Program");
+        }
+    }
+}
