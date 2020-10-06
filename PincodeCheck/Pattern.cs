b25895eda5d4450b4b22befd11b1896f1ed6ff64
Administrator <siddhiseth3009@gmail.com>
2020-10-06 20:04:32 +0530

    [Siddhi]Add Pattern class added with pincodeValidation method

---
+++ PincodeCheck/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PincodeCheck
+{
+    class Pattern
+    {
+        public static string REGEX_PINCODE = "^[1-9][0-9]{5}$";//400088
+        public bool validatePincode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+    }
+}
--- PincodeCheck/Program.cs
+++ PincodeCheck/Program.cs
@@ -7,6 +7,11 @@ namespace PincodeCheck
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Pincode Check Program");
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter you pincode");
+            string pincode = Console.ReadLine();
+            bool valid = pattern.validatePincode(pincode);
+            Console.WriteLine(valid);
         }
     }
 }
